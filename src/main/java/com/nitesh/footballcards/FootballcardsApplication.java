b093d537365717150ad02ab1fa2a510754b6005e
NiteshSangroula <sangroulanitesh123@gmail.com>
2025-09-12 12:00:50 +0545

    initial commit

---
+++ src/main/java/com/nitesh/footballcards/FootballcardsApplication.java
@@ -0,0 +1,13 @@
+package com.nitesh.footballcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitesh/footballcards/FootballcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitesh.footballcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
